4e1030f290cf1a48d1501d8a17d9d2b11e541c3d
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-10 14:57:53 +0100

    new class WorldImpl that implements World

--- src/main/java/it/unibo/tankBattle/model/world/api/World.java
+++ src/main/java/it/unibo/tankBattle/model/world/api/World.java
@@ -6,7 +6,7 @@ import it.unibo.tankBattle.model.gameObject.api.GameObject;
 public interface World {
     public Stream<GameObject> getEntities();
 
-    public void removeGameObject(GameObject gameObject);
+    public void removeGameObject(final GameObject gameObject);
 
-    public void addGameObject(GameObject addGameObject);
+    public void addGameObject(final GameObject gameObject);
 }
