3c3b202b31d34855ea68ae21bfbc8a87677ea0ca
Emanuele_Martelli <emanuele.martelli5studio.unibo.it>
2023-03-10 14:28:15 +0100

    pull down

--- src/main/java/it/unibo/tankBattle/TankBattle.java
+++ src/main/java/it/unibo/tankBattle/TankBattle.java
@@ -1,7 +1,21 @@
 package it.unibo.tankBattle;
 
+import it.unibo.tankBattle.GameObject.common.Directions;
+import it.unibo.tankBattle.GameObject.common.Point2D;
+import it.unibo.tankBattle.GameObject.impl.GameObjectFactoryImpl;
+import it.unibo.tankBattle.GameObject.impl.LifepointsComponent;
+import it.unibo.tankBattle.GameObject.impl.MovingComponent;
+import it.unibo.tankBattle.GameObject.api.GameObjectFactory;
+
 public class TankBattle {
-    public static void main (String[] arg) {
-        System.out.println("Heello World!");
+    public static void main(String[] arg) {
+        GameObjectFactory factory = new GameObjectFactoryImpl();
+        var tank = factory.createTank(new Point2D(10, 10), Directions.UP);
+        var bullet = factory.createBullet(new Point2D(0, 0));
+        tank.setDirection(Directions.DOWN);
+        System.out.println(tank.getDirection());
+        tank.getComponent(MovingComponent.class).ifPresent(c -> System.out.println(c.getSpeed()));
+        bullet.getComponent(LifepointsComponent.class).ifPresent(c -> System.out.println(c.getLifePoints()));
+
     }
 }
